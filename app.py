b61c01dbf0a2794df6bb56c942cd7e055569834c
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2025-09-13 12:24:14 +0100

    mitigate cors errors

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-# existing local venv: source ../3-simple-chatbot/.venv/bin/activate ()
+# existing local venv: source ../3-simple-chatbot/.venv/bin/activate
 # for new env: python -m venv .venv ; source .venv/bin/activate
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route("/")
 def home():
